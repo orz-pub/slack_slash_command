c9ab2771f63f4d8622457d7a21afa27d32938dcb
bwv21 <jw.bwv21@gmail.com>
2018-07-14 15:27:36 +0900

    app.js 버그 수정, test 함수 개선

--- src/SlackSlashCommandSuite/slack_slash_command_lambda/src/commands/test.js
+++ src/SlackSlashCommandSuite/slack_slash_command_lambda/src/commands/test.js
@@ -1,6 +1,20 @@
-﻿"user strict";
+﻿"use strict";
 
 exports.do = function(event, context, end) {
-    context.slackText = "테스트 성공";
+    const args = event.text.split(" ");
+
+    const functionName = args[0];
+
+    if (typeof this[functionName] !== "function") {
+        context.slackText = `/test 에서 지원하지 않는 명령입니다. ${functionName}`;
+        end();
+        return;
+    }
+
+    this[functionName](args, event, context, end);
+}
+
+exports.echo = function (args, event, context, end) {
+    context.slackText = `call ${args}`;
     end();
 }
